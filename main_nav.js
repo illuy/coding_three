3399a50858d10eea7259569fe2ee23145785b83e
illu_y <ii_2002@naver.com>
2023-12-22 14:13:06 +0900

    수정

--- main_nav.js
+++ main_nav.js
@@ -4,14 +4,9 @@ $(window).scroll(function () {
     if (Cscroll >= $('#HOME').offset().top) {
         $('nav li:nth-child(1)').addClass('show').siblings().removeClass('show')
     }
-
     if (Cscroll >= $('#TEAM').offset().top - 200) {
         $('nav li:nth-child(2)').addClass('show').siblings().removeClass('show')
-     
-    } else {
-       
-    }
-
+    } 
     if (Cscroll >= $('#MEMBER').offset().top) {
         $('nav li:nth-child(3)').addClass('show').siblings().removeClass('show')
     }
