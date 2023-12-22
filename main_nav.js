730fee1cf8295474117b59847f677597d3fdbb26
illu_y <ii_2002@naver.com>
2023-12-22 14:15:46 +0900

    수정

--- main_nav.js
+++ main_nav.js
@@ -1,18 +1,20 @@
-$(window).scroll(function () {
-    let Cscroll = $(this).scrollTop()
-
-    if (Cscroll >= $('#HOME').offset().top) {
-        $('nav li:nth-child(1)').addClass('show').siblings().removeClass('show')
-    }
-    if (Cscroll >= $('#TEAM').offset().top - 200) {
-        $('nav li:nth-child(2)').addClass('show').siblings().removeClass('show')
-    } 
-    if (Cscroll >= $('#MEMBER').offset().top) {
-        $('nav li:nth-child(3)').addClass('show').siblings().removeClass('show')
-    }
-
-    if (Cscroll >= $('#BOARD').offset().top) {
-        $('nav li:nth-child(4)').addClass('show').siblings().removeClass('show')
-    }
-
+$(document).ready(function(){
+    $(window).scroll(function () {
+        let Cscroll = $(this).scrollTop()
+    
+        if (Cscroll >= $('#HOME').offset().top) {
+            $('nav li:nth-child(1)').addClass('show').siblings().removeClass('show')
+        }
+        if (Cscroll >= $('#TEAM').offset().top - 200) {
+            $('nav li:nth-child(2)').addClass('show').siblings().removeClass('show')
+        } 
+        if (Cscroll >= $('#MEMBER').offset().top) {
+            $('nav li:nth-child(3)').addClass('show').siblings().removeClass('show')
+        }
+    
+        if (Cscroll >= $('#BOARD').offset().top) {
+            $('nav li:nth-child(4)').addClass('show').siblings().removeClass('show')
+        }
+    
+    })
 })
